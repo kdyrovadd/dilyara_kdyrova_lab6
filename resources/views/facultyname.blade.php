027eddaf381c8f3eed2b79c39d52ff3b05b2e969
kdyrovadd <190103436@stu.sdu.edu.kz>
2021-02-20 19:34:32 +0300

    initial commit

---
+++ resources/views/facultyname.blade.php
@@ -0,0 +1,3 @@
+<?php
+	echo 'Engineering and natural science';
+?>
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/name', function () {
+    return view('myname');
+});
+
+Route::get('/faculty', function () {
+    return view('facultyname');
+});
+
+Route::get('/post/{id}', function ($id) {
+    return 'id number is: '.$id;
+});
+
+Route::get('u/{name?}', function ($name=null) {
+    return $name;
+})->where('name','[a-zA-Z]+');
