8f93e807ce5ebf67dfeeb1544d08d5b98abb51b5
kdyrovadd <190103436@stu.sdu.edu.kz>
2021-03-05 09:40:02 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -39,4 +40,60 @@ Route::get('/students/{id}', "StudentController@show");
 //this route return string "lab4"
 Route::get('/lab', function() {
 	return 'lab4';
+});
+
+Route::get('/insert', function () {
+    DB::insert("insert into students(name, date_of_birth, GPA, advisor) values('Dilyara', '2020-10-27', 3.81, 'Kurmangazy K')");
+});
+
+Route::get('/select', function () {
+    $results = DB::select("select * from students");
+    foreach($results as $i) {
+    	echo "Name is ".$i->name."<br>";
+        echo "Date of birth is ".$i->date_of_birth."<br>";
+    	echo "GPA is ".$i->GPA."<br>";
+    	echo "Advisor is ".$i->advisor."<br>";
+        echo "<br>";
+    }
+});
+
+Route::get('/update', function () {
+    $updated = DB::update("update students set advisor = 'Aigul B' where id = 1");
+    return $updated;
+});
+
+Route::get('/delete', function () {
+    $deleted = DB::delete("delete from students where id = 1");
+    return $deleted;
+});
+
+Route::get('/insert2', function () {
+    $st=new Student;
+    $st->name='Aigul';
+    $st->date_of_birth='2002-06-02';
+    $st->GPA=3.61;
+    $st->advisor='Aidar S';
+    $st->save();
+});
+
+Route::get('/update2', function () {
+    $st=Student::find(3);
+    $st->GPA=3.65;
+    $st->save();
+});
+
+Route::get('/delete2', function() {
+    Student::destroy(3);
+});
+
+Route::get('select2', function() {
+    $st=Student::all();
+    foreach ($st as $i) {
+        echo "<b>$i->id</b>".". ";
+        echo $i->name." <br>";
+        echo $i->date_of_birth." <br>";
+        echo $i->GPA." <br>";
+        echo $i->advisor." <br>";
+        echo "<br>";
+    }
 });
